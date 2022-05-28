b2faa99a2ecb700a8c05112a89fb86b610c3c2df
Avrodip Saha <95330949+Avrodipsaha@users.noreply.github.com>
2022-05-28 18:27:28 +0530

    Update porfolio.js

--- porfolio.js
+++ porfolio.js
@@ -1,42 +1,41 @@
-const buttenCliked = document.querySelector(".butten-cliked");
-buttenCliked.addEventListener("click",() =>{
-    hideSection();
-    toggleNavbar();
-});
-function hideSection(){
-    document.querySelector("section.active").classList.toggle("fade-out");
+function hideSection() {
+  document.querySelector("section.active").classList.toggle("fade-out");
 }
-function toggleNavbar(){
-document.querySelector(".header").classList.toggle("active");
+function toggle() {
+  document.querySelector(".header").classList.toggle("active");
 }
 
 /*-------Active section------*/
-document.addEventListener("click", (e) =>{
-    if(e.target.classList.contains("link-item") && e.target.hash !== ""){
-        //active the overly to privent the multiple clicks
-        document.querySelector(".overlay").classList.add("active");
-        buttenCliked.classList.add("hide")
-        else{
-             hideSection();
-        }
-        setTimeout(() =>{
-            document.querySelector("section.active").classList.remove("active","fade-out");
-            document.querySelector(e.target.hash).classList.add("active");
-            window.scrollTo(0,0);
-        buttenCliked.classList.remove("hide");
-        document.querySelector(".overlay").classList.remove("active");
-        },500)
-    }
-})
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+    if (e.target.classList.contains("nav-item")) toggle();
+    else hideSection();
+
+    setTimeout(() => {
+      document
+        .querySelector("section.active")
+        .classList.remove("active", "fade-out");
+      document.querySelector(e.target.hash).classList.add("active");
+      window.scrollTo(0, 0);
+    }, 500);
+  }
+});
 
 /*-------About tabs------*/
-const tabsContainer = document.querySelector(".about-tabs"),
+const tabsContainer = document.querySelector(".about-tabs");
 const aboutSection = document.querySelector(".about-section");
 
-tabsContainer.addEventListener("click" , (e) =>{
-    if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
-        tabsContainer.querySelector(".active").classList.remove("active");
-        e.target.classList.add("active");
-        aboutSection.querySelector(".tab-content.active").classList.remove("active");
-    }
+tabsContainer.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("tab-item") &&
+    !e.target.classList.contains("active")
+  ) {
+    tabsContainer.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    const target = e.target.getAttribute("data-target");
+    aboutSection
+      .querySelector(".tab-content.active")
+      .classList.remove("active");
+    aboutSection.querySelector(target).classList.add("active");
+  }
 });
