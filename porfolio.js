17302bdca8750c817d4114f92373e22df8b84ab8
Avrodip Saha <95330949+Avrodipsaha@users.noreply.github.com>
2022-05-28 17:39:54 +0530

    Update porfolio.js

--- porfolio.js
+++ porfolio.js
@@ -2,7 +2,6 @@ const buttenCliked = document.querySelector(".butten-cliked");
 buttenCliked.addEventListener("click",() =>{
     hideSection();
     toggleNavbar();
-    document.body.classList.toggle("hide-scrolling");
 });
 function hideSection(){
     document.querySelector("section.active").classList.toggle("fade-out");
@@ -17,38 +16,27 @@ document.addEventListener("click", (e) =>{
         //active the overly to privent the multiple clicks
         document.querySelector(".overlay").classList.add("active");
         buttenCliked.classList.add("hide")
-        if(e.target.classList.contains("nav-item")){
-            toggleNavbar();
-        }
         else{
              hideSection();
-            document.body.classList.add("hide-scrolling");
-
         }
         setTimeout(() =>{
             document.querySelector("section.active").classList.remove("active","fade-out");
             document.querySelector(e.target.hash).classList.add("active");
             window.scrollTo(0,0);
-            document.body.classList.remove("hide-scrolling");
-        buttenCliked.classList.remove("hide")
+        buttenCliked.classList.remove("hide");
         document.querySelector(".overlay").classList.remove("active");
-
-
         },500)
     }
 })
 
 /*-------About tabs------*/
 const tabsContainer = document.querySelector(".about-tabs"),
-aboutSection = document.querySelector(".about-section");
+const aboutSection = document.querySelector(".about-section");
 
 tabsContainer.addEventListener("click" , (e) =>{
     if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
         tabsContainer.querySelector(".active").classList.remove("active");
         e.target.classList.add("active");
-        const target = e.target.getAttribute("data-target");
         aboutSection.querySelector(".tab-content.active").classList.remove("active");
-        aboutSection.querySelector(target).classList.add("active");
-
     }
 });
